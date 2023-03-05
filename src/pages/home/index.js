c8486ca276128a91c6c390918b9149e87004f0d7
6410110429 <6410110429@psu.ac.th>
2023-03-05 23:53:02 +0700

    Edit Login Page and Create Chat

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import styles from './styles.css';
 import { useNavigate } from 'react-router-dom'; // Add this
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -11,75 +11,52 @@ import { height, padding } from '@mui/system';
 
 const Home = ({ username, setUsername, room, setRoom, socket }) => {
     const navigate = useNavigate(); // Add this
-
     const joinRoom = () => {
-      if (room !== '' && username !== '') {
-        socket.emit('join_room', { username, room });
-      }
-  
-      // Redirect to /chat
-      navigate('/chat', { replace: true }); // Add this
-    };
-  return (
-        <div className='Box'>
+        if (room !== '' && username !== '') {
+            socket.emit('join_room', { username, room });
+        }
+        navigate('/chat', { replace: true }); // Add this
 
+    };
+    return (
+        <div className='Box'>
             <div className='TitleBox'>
                 COE Chat
             </div>
             <div className='form'>
-            <FormControl fullWidth>
-                <TextField
-                id="outlined-controlled"
-                label="Username"
-                onChange={(e) => setUsername(e.target.value)}   
-                />
+                <FormControl fullWidth>
+                    <TextField
+                        id="outlined-controlled"
+                        label="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </FormControl>
             </div>
             <div className='form'>
-            <FormControl fullWidth>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Room</InputLabel>
+                    <Select
+                        label="Room"
+                        onChange={(e) => setRoom(e.target.value)}
+                    >
+                        <MenuItem value={'Room01'}>Room 01</MenuItem>
+                        <MenuItem value={'Room02'}>Room 02</MenuItem>
 
-            <InputLabel id="demo-simple-select-label">Room</InputLabel>
-                <Select
-                label="Room"
-                onChange={(e) => setRoom(e.target.value)}
-                >
-                </Select>
+                    </Select>
                 </FormControl>
             </div>
             <Box textAlign='center' paddingTop={5}>
-            <Button
-             variant="contained"
-             onClick={joinRoom}
-             className="buttonjoin"
-             sx={{height:60, width:200,borderRadius:5}}
-            >
-                <div style={{fontSize:"14pt"}}>Join Room</div>
-            </Button>
+                <Button
+                    variant="contained"
+                    onClick={joinRoom}
+                    className="buttonjoin"
+                    sx={{ height: 60, width: 200, borderRadius: 5 }}
+                >
+                    <div style={{ fontSize: "14pt" }}>Join Room</div>
+                </Button>
             </Box>
-
-
-        
         </div>
-      );
-
-    // <div className="container">
-    //   // ...
-    //     <input
-    //       className="input"
-    //       placeholder='Username...'
-    //       onChange={(e) => setUsername(e.target.value)} // Add this
-    //     />
-
-    //     <select
-    //       className="input"
-    //       onChange={(e) => setRoom(e.target.value)} // Add this
-    //     >
-    //      // ...
-    //     </select>
-
-    //     // ...
-    // </div>
-
+    );
 };
 
 export default Home;
